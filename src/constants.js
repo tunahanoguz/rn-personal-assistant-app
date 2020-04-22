696763ec2af7ec7ffc29367a26c0da9df8eae766
tunahanoguz <tunahanog@gmail.com>
2020-04-22 19:39:56 +0300

    Created constants for Conversation screen messages.

---
+++ src/constants.js
@@ -0,0 +1,10 @@
+export const messages = [
+  { id: 1, text: 'GM Paolo, how can help you today?' },
+  {
+    id: 2,
+    text:
+      'Show me Nikolas email from last month, the one which talk about the Summer Design Event in Seattle',
+    isSent: true,
+  },
+  { id: 3, text: 'Sure, sending it right now...' },
+];
