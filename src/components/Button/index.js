ef96b2884d471a05f83bd73b529496cb82929bf6
tunahanoguz <tunahanog@gmail.com>
2020-04-22 15:49:37 +0300

    Added new styles inside Button component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,21 +1,36 @@
 import React from 'react';
 import { TouchableOpacity } from 'react-native';
+import PropTypes from 'prop-types';
+import { colors } from '../../styles';
 
 function Button(props) {
-  const { bgColor, children } = props;
+  const { bgColor, style, children } = props;
 
-  const style = [
+  const buttonStyle = [
     bgColor && { backgroundColor: bgColor },
     {
+      justifyContent: 'center',
+      alignItems: 'center',
+      paddingVertical: 16,
+      borderRadius: 24,
       shadowColor: '#4E4F72',
       shadowOpacity: 0.08,
       shadowRadius: 60,
       shadowOffset: { width: 0, height: 4 },
       elevation: 60,
     },
+    style,
   ];
 
-  return <TouchableOpacity style={style}>{children}</TouchableOpacity>;
+  return <TouchableOpacity style={buttonStyle}>{children}</TouchableOpacity>;
 }
+
+Button.propTypes = {
+  bgColor: PropTypes.string,
+};
+
+Button.defaultProps = {
+  bgColor: colors.darkPurple,
+};
 
 export default Button;
