3238c457d4c006c0e24636b11483fcd1b5661587
tunahanoguz <tunahanog@gmail.com>
2020-04-22 14:29:27 +0300

    Created index.js for exports in components and screen folders, installed react-navigation packages and created initial screen.
    Created index.js for export all files in components and screens folders.
    
    Installed react-navigation and other packages related to this package. And configured that in App.js!
    
    Created an initial screen for navigation named Introduction.

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export Avatar from './Avatar';
+export Button from './Button';
+export Message from './Message';
+export Text from './Text';
---
+++ src/screens/Introduction/index.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Text, Button } from '../../components';
+
+function Introduction() {
+  return (
+    <Button>
+      <Text>Hey!</Text>
+    </Button>
+  );
+}
+
+export default Introduction;
