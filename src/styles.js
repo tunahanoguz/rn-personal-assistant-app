bf949408ed98fb594209312c7b429f74dd543b13
tunahanoguz <tunahanog@gmail.com>
2020-04-22 12:18:25 +0300

    Created styles file.
    Created styles file for global styling. This file is including colors and text sizes.

---
+++ src/styles.js
@@ -0,0 +1,35 @@
+export const colors = {
+  background: '#FAFBFD',
+  darkPurple: '#7041EE',
+  lightPurple: '#7041EE',
+  extraLightPurple: '#E8FAF3',
+  red: '#FA4F3B',
+  darkGreen: '#1ECD8C',
+  lightGreen: '#E8FAF3',
+  yellow: '#FECD56',
+  grey: '#E8FAF3',
+  blue: '#4285F4',
+  black: '#2C2929',
+  white: '#FFFFFF',
+};
+
+export const fonts = {
+  extraLarge: {
+    fontSize: 60,
+  },
+  large: {
+    fontSize: 36,
+  },
+  lowerLarge: {
+    fontSize: 24,
+  },
+  medium: {
+    fontSize: 20,
+  },
+  normal: {
+    fontSize: 18,
+  },
+  small: {
+    fontSize: 16,
+  },
+};
