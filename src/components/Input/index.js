f42ebb90a71293548bc1f2cff784fabd84d15f50
tunahanoguz <tunahanog@gmail.com>
2020-04-22 17:47:50 +0300

    Completed Input component.
    Added outer view and styles.

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import { TextInput, StyleSheet } from 'react-native';
+import { View, TextInput, StyleSheet } from 'react-native';
 import PropTypes from 'prop-types';
 import { colors } from '../../styles';
 
 function Input(props) {
-  const { value, placeholder } = props;
-  return <TextInput placeholder={placeholder} value={value} style={styles} />;
+  const { value, placeholder, keyboardType, setValue, isBordered } = props;
+  return (
+    <View style={[styles.outer, isBordered && styles.border]}>
+      <TextInput
+        placeholder={placeholder}
+        value={value}
+        keyboardType={keyboardType}
+        onChangeText={(text) => setValue(text)}
+        style={styles.input}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  paddingVertical: 16,
-  backgroundColor: colors.white,
-  borderWidth: 1,
-  borderColor: colors.darkGrey,
-  fontFamily: 'CircularStd-Book',
-  fontSize: 20,
-  color: colors.darkGrey,
+  outer: {
+    marginTop: 16,
+    paddingVertical: 2,
+    paddingLeft: 24,
+    backgroundColor: colors.white,
+    borderRadius: 24,
+    shadowColor: '#4E4F72',
+    shadowOpacity: 0.08,
+    shadowRadius: 60,
+    shadowOffset: { width: 0, height: 4 },
+    elevation: 60,
+  },
+  input: {
+    fontFamily: 'CircularStd-Book',
+    fontSize: 20,
+    color: colors.darkGrey,
+  },
+  border: {
+    borderWidth: 1,
+    borderColor: colors.darkGrey,
+  },
 });
 
 Input.propTypes = {
   value: PropTypes.string.isRequired,
   placeholder: PropTypes.string.isRequired,
+  keyboardType: PropTypes.string,
+  setValue: PropTypes.func.isRequired,
+  isBordered: PropTypes.bool,
+};
+
+Input.defaultProps = {
+  keyboardType: 'default',
+  isBordered: false,
 };
 
 export default Input;
