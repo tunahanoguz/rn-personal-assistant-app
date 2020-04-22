dcc01151f51f969c1e644b50f787c1a89b694170
tunahanoguz <tunahanog@gmail.com>
2020-04-22 19:43:08 +0300

    Added multiline and onFocus properties to Input component.

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -4,16 +4,35 @@ import PropTypes from 'prop-types';
 import { colors } from '../../styles';
 
 function Input(props) {
-  const { value, placeholder, keyboardType, setValue, isBordered } = props;
+  const {
+    value,
+    placeholder,
+    keyboardType,
+    setValue,
+    onFocus,
+    isBordered,
+    isShadow,
+    isMultiline,
+    style,
+    children,
+  } = props;
   return (
-    <View style={[styles.outer, isBordered && styles.border]}>
+    <View
+      style={[
+        styles.outer,
+        isBordered && styles.border,
+        isShadow && styles.shadow,
+      ]}>
       <TextInput
         placeholder={placeholder}
         value={value}
         keyboardType={keyboardType}
+        multiline={isMultiline}
         onChangeText={(text) => setValue(text)}
-        style={styles.input}
+        onFocus={onFocus}
+        style={[styles.input, style]}
       />
+      {children}
     </View>
   );
 }
@@ -25,11 +44,6 @@ const styles = StyleSheet.create({
     paddingLeft: 24,
     backgroundColor: colors.white,
     borderRadius: 24,
-    shadowColor: '#4E4F72',
-    shadowOpacity: 0.08,
-    shadowRadius: 60,
-    shadowOffset: { width: 0, height: 4 },
-    elevation: 60,
   },
   input: {
     fontFamily: 'CircularStd-Book',
@@ -38,7 +52,14 @@ const styles = StyleSheet.create({
   },
   border: {
     borderWidth: 1,
-    borderColor: colors.darkGrey,
+    borderColor: colors.lightGrey,
+  },
+  shadow: {
+    shadowColor: '#4E4F72',
+    shadowOpacity: 0.08,
+    shadowRadius: 60,
+    shadowOffset: { width: 0, height: 4 },
+    elevation: 60,
   },
 });
 
@@ -46,13 +67,18 @@ Input.propTypes = {
   value: PropTypes.string.isRequired,
   placeholder: PropTypes.string.isRequired,
   keyboardType: PropTypes.string,
+  multiline: PropTypes.bool,
   setValue: PropTypes.func.isRequired,
+  onFocus: PropTypes.func,
   isBordered: PropTypes.bool,
+  isShadow: PropTypes.bool,
 };
 
 Input.defaultProps = {
   keyboardType: 'default',
+  multiline: false,
   isBordered: false,
+  isShadow: true,
 };
 
 export default Input;
